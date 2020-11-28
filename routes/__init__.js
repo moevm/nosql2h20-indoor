8f7d58f698d1953e9d5b7452a3079a3e46c3fe1b
ravlasov <ravlasov@yandex.ru>
2020-11-29 00:05:31 +0300

    import fix errors on existing documents

--- routes/__init__.js
+++ routes/__init__.js
@@ -1,43 +1,31 @@
 let router = require('express').Router();
 const path = require('path');
+const upload = require(path.join(appRoot, 'modules/multer_init'))
 
-function add_route(method, path, file) {
+function add_route(method, path, file, options) {
     switch (method.toUpperCase()) {
         case "GET":
-            router.get(path, require(file));
+            router.get(path, options, require(file));
             break;
         case "POST":
-            router.post(path, require(file));
+            router.post(path, options, require(file));
             break;
         case "PUT":
-            router.put(path, require(file));
+            router.put(path, options, require(file));
             break;
         default:
-            router.use(path, require(file));
+            router.use(path, options, require(file));
     }
     console.log(`Initialized ${method} route at '${path}' from file '${file.replace('.', './routes')}'`);
 }
 
 // GET
-add_route('GET', '/', path.join(appRoot, 'routes/get/index'));
+add_route('GET', '/', path.join(appRoot, 'routes/get/index'), upload.none());
 
 // POST
-add_route('POST', '/search', path.join(appRoot, 'routes/post/search'));
+add_route('POST', '/search', path.join(appRoot, 'routes/post/search'), upload.none());
 
-
-const multer = require('multer');
-let storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/')
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + path.extname(file.originalname)) //Appending extension
-    }
-})
-let upload = multer({ storage: storage })
-router.post('/import/file', upload.single('data'), require(path.join(appRoot, 'routes/post/import_file')))
-
-//add_route('POST', '/import/file', path.join(appRoot, 'routes/put/import_file'), upload.single('data'));
+add_route('POST', '/import/file', path.join(appRoot, 'routes/post/import_file'), upload.single('data'));
 
 module.exports = router;
 
--- routes/post/import_file.js
+++ routes/post/import_file.js
@@ -2,29 +2,29 @@ const path = require('path');
 const schemas = require(path.join(appRoot, 'modules/schemas'));
 const Room = schemas.room;
 const Vertex = schemas.vertex;
-const fs = require('fs')
+const fs = require('fs');
 
-module.exports = function (req, res, next) {
-    fpath = path.join(appRoot, req.file.path)
-    let data = require(fpath)
+module.exports = async function (req, res, next) {
+    fpath = path.join(appRoot, req.file.path);
+    let data = require(fpath);
 
     let room_data = data.room;
     for (let i = 0; i < room_data.length; i++) {
-        new Room(room_data[i]).save((err, doc) => {
+        await Room.updateOne({_id: room_data[i]._id}, room_data[i], {upsert: true}, (err, doc) => {
             if (err) console.error(err);
-        });
+        })
     }
     console.log(`${room_data.length} items imported to rooms`);
+
     let vertex_data = data.vertex;
     for (let i = 0; i < vertex_data.length; i++) {
-        new Vertex(vertex_data[i]).save((err, doc) => {
+        Vertex.updateOne({_id: vertex_data[i]._id}, vertex_data[i], {upsert: true}, (err, doc) => {
             if (err) console.error(err);
-        });
+        })
     }
     console.log(`${vertex_data.length} items imported to vertices`);
+
     fs.unlink(fpath, (err) => {
-        if (err) {
-            console.error(err)
-        }
+        if (err) console.error(err);
     })
 };
